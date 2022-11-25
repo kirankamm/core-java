c52df45309623e7885d524dbbc3fa7e2d4335da3
kirankamm <kirankammar.xworkz@gmail.com>
2022-11-25 20:20:04 +0530

    commting method related files

---
+++ UberEatsTester.java
@@ -0,0 +1,14 @@
+class  UberEatsTester1{
+	
+	public static void main(String UberEats[]){
+		
+	System.out.println("the main method started here");
+	
+	//double priceOffood=UberEats.getItem("Pridrice");
+	UberEats1.findItem("Pridrice");
+	
+	//System.out.println("the price is:"+priceofFood);
+	System.out.println("the main method ended here");
+	}
+	
+	}
---
+++ ZomatoTester.java
@@ -0,0 +1,20 @@
+class ZomatoTester {
+
+ public static void main(String zomato[]){
+	 
+	 System.out.println("the main method started here");
+	 
+	 
+	 
+	 //double priceOfFood=Zomato.findItem("Puliogere");
+	 Zomato.findItem("edli");
+	 
+	 
+	 
+       //System.out.println("the price is:"+priceOfFood);
+       System.out.println("the main method ended here");
+  
+  
+   }
+   
+    } 
