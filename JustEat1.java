502f6b59078b28d82b4a51257c6cfbbd413e2630
kirankamm <kirankammar.xworkz@gmail.com>
2022-11-28 16:41:39 +0530

    file added

---
+++ JustEat1.java
@@ -0,0 +1,62 @@
+class  JustEat1{
+  
+    public static void findItem(String item){
+	System.out.println("invoked findItem()");
+	
+	    //"Eggpakoda"=="Kunda"
+	
+	   if(item == "Kunda"){
+		    System.out.println("The Price of Kunda is 200");
+			return;
+	   }
+	   //"Kunda"=="Kunda"
+	    if(item=="Eggpakoda"){
+			System.out.println("The Price of Eggpakoda is 100");
+			return;
+		}
+		else{
+			System.out.println("Item not available");
+		}
+		System.out.println("end of findItem()");
+
+	    }
+        }	
+		
+			
+			
+		   
+		  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Laptop.java
@@ -0,0 +1,18 @@
+class Laptop {
+
+      public static void main (String a[]){
+	  
+	      System.out.println("main method started");
+	      display();
+	      System.out.println("main method ended");
+	   
+	      }
+	   
+	  public static void display(){
+			 
+		  System.out.println("method  calling") ;
+		  System.out.println("method ended") ;
+		 
+		 }
+		 
+		 }
